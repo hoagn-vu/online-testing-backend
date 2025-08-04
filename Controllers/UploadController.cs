f0dc1ee961ada6126eb1e5bdc7bef7cf94f8cae2
phuonglinh81 <akagamishirayuki88@gmail.com>
2025-08-04 16:55:30 +0700

    add image upload function for question

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,25 @@
+﻿using Backend_online_testing.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_online_testing.Controllers
+{
+    public class UploadController : ControllerBase
+    {
+        private readonly S3Service _s3Service;
+
+        public UploadController(S3Service s3Service)
+        {
+            _s3Service = s3Service;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("File không hợp lệ");
+
+            var url = await _s3Service.UploadFileAsync(file);
+            return Ok(new { url });
+        }
+    }
+}
---
+++ Services/S3Service.cs
@@ -0,0 +1,44 @@
+﻿using Amazon.S3;
+using Amazon.S3.Transfer;
+using Microsoft.Extensions.Configuration;
+
+namespace Backend_online_testing.Services
+{
+    public class S3Service
+    {
+        private readonly IAmazonS3 _s3Client;
+        private readonly string _bucketName;
+
+        public S3Service(IConfiguration configuration)
+        {
+            var accessKey = configuration["AWS:AccessKey"];
+            var secretKey = configuration["AWS:SecretKey"];
+            var region = configuration["AWS:Region"];
+            _bucketName = configuration["AWS:BucketName"];
+
+            _s3Client = new AmazonS3Client(accessKey, secretKey, Amazon.RegionEndpoint.GetBySystemName(region));
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            var fileTransferUtility = new TransferUtility(_s3Client);
+
+            using (var stream = file.OpenReadStream())
+            {
+                var fileKey = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                var uploadRequest = new TransferUtilityUploadRequest
+                {
+                    InputStream = stream,
+                    Key = fileKey,
+                    BucketName = _bucketName,
+                    ContentType = file.ContentType,
+                    //CannedACL = S3CannedACL.PublicRead
+                };
+
+                await fileTransferUtility.UploadAsync(uploadRequest);
+                return $"https://{_bucketName}.s3-ap-southeast-2.amazonaws.com/{fileKey}";
+            }
+        }
+    }
+}
