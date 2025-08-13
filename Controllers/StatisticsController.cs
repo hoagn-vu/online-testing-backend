08e92ac059cfa55b1180209c6025209813cd0d64
phuonglinh81 <akagamishirayuki88@gmail.com>
2025-08-13 13:21:48 +0700

    Update StatisticsController.cs

---
+++ Controllers/StatisticsController.cs
@@ -0,0 +1,37 @@
+﻿using Backend_online_testing.Services;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_online_testing.Controllers;
+
+[Route("api/statistics")]
+[ApiController]
+public class StatisticsController : ControllerBase
+{
+    private readonly StatisticsService _statisticsService;
+
+    public StatisticsController(StatisticsService statisticService)
+    {
+        _statisticsService = statisticService;
+    }
+
+    [HttpGet("organize-exam-by-id")]
+    public async Task<IActionResult> StatisticsOrganizeExam(string organizeExamId)
+    {
+        if (string.IsNullOrWhiteSpace(organizeExamId))
+            return BadRequest("organizeExamId is required.");
+
+        var result = await _statisticsService.GetScoreHistogram10Async(organizeExamId);
+        return Ok(result);
+    }
+
+    [HttpGet("participation-violation-by-id")]
+    public async Task<IActionResult> GetParticipationViolation(string organizeExamId)
+    {
+        if (string.IsNullOrWhiteSpace(organizeExamId))
+            return BadRequest("organizeExamId is required.");
+
+        var result = await _statisticsService.GetParticipationViolationAsync(organizeExamId);
+        return Ok(result);
+    }
+}
